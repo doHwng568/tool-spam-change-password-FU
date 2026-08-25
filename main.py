b93e46439dc1405b87ed3a2113cf63f1db5ddd85
doHung568 <contact.me.dothehung@gmail.com>
2026-08-25 23:54:28 +0700

    function find input

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-def open_web():
-    driver = webdriver.Chrome()
+# 1 driver tương ứng với 1 session trên browser
+# khai báo biến này là global để các function có thể thao tác cùng trên 1 session web đó
+driver = webdriver.Chrome()
 
-    try:
-        driver.get("https://chgpwd.fpt.edu.vn")
-
-    finally:
-        print('ok')
+def open_web(x):
+        x.get("https://chgpwd.fpt.edu.vn")
+        print('open web success')
 
 
-open_web()
+def find_input(x):
+    
+    inputs = x.find_elements(By.TAG_NAME, "input")
+    
+    print('Amount of input: ', len(inputs))
+
+    for element in inputs:
+        print(
+            "id =", element.get_attribute("id"),
+            "| name =", element.get_attribute("name"),
+            "| type =", element.get_attribute("type"),
+            "| placeholder =", element.get_attribute("placeholder")
+        )
+
+open_web(driver)
+
+find_input(driver)
